7d581a18177cf4ecfa5fedfeae71d73ffc02aea8
ansariashif14@gmail.com <ansariashif14@gmail.com>
2025-08-16 14:30:31 +0530

    corse fix

--- src/main/java/com/inventory/inventory_backend/config/CorsConfig.java
+++ src/main/java/com/inventory/inventory_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://inventory-backend-zwni.onrender.com") // React frontend
+                        .allowedOrigins("https://inventory-backend-zwni.onrender.com", "http://localhost:5173/") // React frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
